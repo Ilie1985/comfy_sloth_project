1d7e9309bee59dbc0ed918273fae1d5eae30aa0c
Ilie1985 <ilie.marian32@yahoo.com>
2023-01-26 10:52:07 +0000

    fech prducts log

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -41,16 +41,16 @@ export const ProductsProvider = ({ children }) => {
   };
 
  ////////////////
-  // const fetchProducts = async (url) => {
-  //   dispatch({ type: GET_PRODUCTS_BEGIN })
-  //   try {
-  //     const response = await axios.get(url)
-  //     const products = response.data
-  //     dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products })
-  //   } catch (error) {
-  //     dispatch({ type: GET_PRODUCTS_ERROR })
-  //   }
-  // }
+  const fetchProducts = async (url) => {
+    dispatch({ type: GET_PRODUCTS_BEGIN })
+    try {
+      const response = await axios.get(url)
+      const products = response.data
+      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products })
+    } catch (error) {
+      dispatch({ type: GET_PRODUCTS_ERROR })
+    }
+  }
 
   // const fetchSingleProduct = async (url) => {
   //   dispatch({ type: GET_SINGLE_PRODUCT_BEGIN })
@@ -63,9 +63,9 @@ export const ProductsProvider = ({ children }) => {
   //   }
   // }
 
-  // useEffect(() => {
-  //   fetchProducts(url)
-  // }, [])
+  useEffect(() => {
+    fetchProducts(url)
+  }, [])
 //////////////////////
   return (
     <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
